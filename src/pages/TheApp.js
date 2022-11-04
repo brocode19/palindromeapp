5ff491b419a7528a8854f41210546140f6664bd0
tinashe <karimugwagwanorkias@gmail.com>
2022-11-04 18:57:38 +0200

    made it responsive

--- src/pages/TheApp.js
+++ src/pages/TheApp.js
@@ -1,37 +1,41 @@
 import { Button, Container, Grid, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import Stack from '@mui/material/Stack';
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
 
 const TheApp = () => {
+
   const [name, setName] = React.useState("Never odd or even");
-  const [reversed,setReversed] = useState('');
+  const [reversed, setReversed] = useState("");
+  const [active, setActive] = useState(false);
+  const [palindrom, setPalindrome] = useState(false);
+
   const handleChange = (event) => {
     setName(event.target.value);
-  }
+    setActive(false);
+  };
 
-  
-  
-  const handleSubmit = () =>{
+  const handleSubmit = () => {
+    setActive(true);
 
-    const preprocessing_regex = /[^a-zA-Z0-9]/g
-    const processed_string = name.toLowerCase().replace(preprocessing_regex , "")
-    const integrity_check = processed_string.split("").reverse().join("")
-    setReversed(integrity_check)
-    if(processed_string === integrity_check){
-      console.log('something');
-    }else{
-      console.log('it does work');
+    const preprocessing_regex = /[^a-zA-Z0-9]/g;
+    const processed_string = name
+      .toLowerCase()
+      .replace(preprocessing_regex, "");
+    const integrity_check = processed_string.split("").reverse().join("");
+    setReversed(integrity_check);
+    if (processed_string === integrity_check) {
+      setPalindrome(true);
+    } else {
+      setPalindrome(false);
     }
-
-
-  }
+  };
   return (
     <div className="mt-5">
       <Container maxWidth="xl">
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={6}>
+          <Grid item xs={12} sm={12} md={8} lg={6}>
             <Typography variant="h4" sx={{ mb: 2 }}>
               Never odd or even: A Coding Project
             </Typography>
@@ -41,7 +45,7 @@ const TheApp = () => {
               run.
             </p>
           </Grid>
-          <Grid item xs={12} sm={6} md={6}>
+          <Grid item xs={12} sm={12} md={4} lg={6}>
             <b>Enter a phrase or word to check</b>
             <TextField
               margin="normal"
@@ -53,19 +57,26 @@ const TheApp = () => {
               value={name}
               onChange={handleChange}
             />
-            <Button className="mb-3" onClick={handleSubmit} variant="contained">check</Button>
-            <Stack sx={{ width: '100%' }} spacing={2}>
-
-
-      <Alert severity="info">
-        <AlertTitle>Info</AlertTitle>
-        This is an info alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="success">
-        <AlertTitle>Well done you have entered a Palindrome</AlertTitle>
-        your string reversed is <strong>{reversed}</strong>
-      </Alert>
-    </Stack>
+            <Button className="mb-3" onClick={handleSubmit} variant="contained">
+              check
+            </Button>
+            {active && (
+              <Stack sx={{ width: "100%" }} spacing={2}>
+                {palindrom ? (
+                  <Alert severity="success">
+                    <AlertTitle>
+                      Well done you have entered a Palindrome
+                    </AlertTitle>
+                    your string reversed is <strong>{reversed}</strong>
+                  </Alert>
+                ) : (
+                  <Alert severity="info">
+                    <AlertTitle>Info</AlertTitle>
+                    <strong>{name}</strong> is not a Palindrome
+                  </Alert>
+                )}
+              </Stack>
+            )}
           </Grid>
         </Grid>
       </Container>
